5f682e32a91a5148dca7c75b3ffa5896c8522b5a
VinitKcse <22bcs15523@cuchd.in>
2025-03-13 12:17:46 +0530

    Exp-6 Medium Level

--- ProductProcessor.java
+++ ProductProcessor.java
@@ -5,7 +5,6 @@ class Product {
     private String name;
     private String category;
     private double price;
-
     public Product(String name, String category, double price) {
         this.name = name;
         this.category = category;
